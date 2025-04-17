2f962a17e63fa05aeebf5d1c3b37d4f5c8b6f0a0
RodrigoF-Lara <rodrigo.lara@rede.ulbra.br>
2025-04-17 10:35:06 -0300

    Update regioes-config.js

--- regioes-config.js
+++ regioes-config.js
@@ -3,25 +3,21 @@ const configuracoesRegioes = {
     id: 'rs_sul',
     nome: 'RS Sul',
     planilhaId: '1R7cj2ajVFQTRSWLNKdY1d1JNVhAjfFfsMvIWKeIhwiA',
-    geojsonPath: 'municipios-RS SUL.geojson',
+    geojsonPath: 'municipios-RS-SUL.geojson', // Nome corrigido
     view: [-30.0346, -51.2177],
     zoom: 6,
     cidadesRC: {
       '1400100': 'NABOR',
-      '1504208': 'FABRÍCIO',
-      // ... adicione todos os RCs da região Sul
+      '1504208': 'FABRÍCIO'
     }
   },
   'rs_norte': {
     id: 'rs_norte',
     nome: 'RS Norte',
     planilhaId: 'ID_DA_PLANILHA_NORTE',
-    geojsonPath: 'municipios-RS NORTE'.geojson'',
+    geojsonPath: 'municipios-RS-NORTE.geojson', // Sintaxe corrigida
     view: [-28.5000, -53.0000],
     zoom: 6,
-    cidadesRC: {
-      // ... adicione os RCs da região Norte
-    }
+    cidadesRC: {}
   }
-  // Adicione as outras 19 regiões seguindo o mesmo padrão
 };
