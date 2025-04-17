fad130a06e31a1919eb54a2b4125c07c475fc787
RodrigoF-Lara <rodrigo.lara@rede.ulbra.br>
2025-04-17 07:44:31 -0300

    Create regioes-config.js

---
+++ regioes-config.js
@@ -0,0 +1,17 @@
+const configuracoesRegioes = {
+  'rs_sul': {
+    id: 'rs_sul',
+    nome: 'RS Sul',
+    planilhaId: '1R7cj2ajVFQTRSWLNKdY1d1JNVhAjfFfsMvIWKeIhwiA',
+    geojsonPath: 'municipios-RS SUL.geojson',
+    view: [-30.0346, -51.2177],
+    zoom: 6
+  },
+  'rs_norte': {
+    id: 'rs_norte',
+    nome: 'RS Norte',
+    planilhaId: 'ID_DA_PLANILHA_NORTE',
+    geojsonPath: 'municipios-RS-Norte.geojson',
+    view: [-28.5000, -53.0000],
+    zoom: 6
+  }
