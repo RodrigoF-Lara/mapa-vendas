7b6d55967caf96a5253b87f5f50f262e17d79f48
RodrigoF-Lara <rodrigo.lara@rede.ulbra.br>
2025-04-18 00:11:11 -0300

    Atualizar o api.js

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,20 +1,29 @@
-// api.js
-export function carregarDadosAPI(sheetId, apiKey) {
+export function carregarDadosAPI(sheetId, apiKey, callback) {
   const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/A1:Z1000?key=${apiKey}`;
-  
-  return fetch(url)
-    .then(response => response.json())
+
+  fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Erro ao carregar dados da API: ${response.status}`);
+      }
+      return response.json();
+    })
     .then(data => {
       if (data.values) {
         const headers = data.values[0];
-        return data.values.slice(1).map(row => {
+        const dadosCSV = data.values.slice(1).map(row => {
           return headers.reduce((obj, header, index) => {
             obj[header] = row[index] || '';
             return obj;
           }, {});
         });
+
+        // Executar a função de callback com os dados processados
+        if (callback) {
+          callback(dadosCSV);
+        }
       } else {
-        throw new Error('Nenhum dado encontrado na planilha.');
+        console.error('Nenhum dado encontrado na planilha.');
       }
     })
     .catch(error => console.error('Erro ao carregar dados da API:', error));
