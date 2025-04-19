8381c574ef9fa98031d51b191bff1a8bc535622b
RodrigoF-Lara <rodrigo.lara@rede.ulbra.br>
2025-04-19 14:52:42 -0300

    Update regioes-config.js

--- config/regioes-config.js
+++ config/regioes-config.js
@@ -25,7 +25,7 @@ const configuracoesRegioes = {
      },
     imagem: 'data/rc/Jeison.PNG',
     marcadorIcone: 'data/rc/marcador_Jeison.svg'
-  };
+  },
   
   'sc': {
     id: 'sc',
@@ -39,7 +39,7 @@ const configuracoesRegioes = {
     },
     imagem: 'data/rc/Jeison.PNG',
     marcadorIcone: 'data/rc/marcador_Jeison.svg'
-  };
+  },
   
   'pr': {
     id: 'pr',
@@ -53,7 +53,7 @@ const configuracoesRegioes = {
      },
     imagem: 'data/rc/Jeison.PNG',
     marcadorIcone: 'data/rc/marcador_Jeison.svg'
-  };
+  },
   
   'sp': {
     id: 'sp',
