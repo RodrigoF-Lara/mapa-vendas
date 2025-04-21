19b4dd062b719ea0c57ef021468cbd1b41025dd2
RodrigoF-Lara <rodrigo.lara@rede.ulbra.br>
2025-04-21 19:35:13 -0300

    Update regioes-config.js

--- config/regioes-config.js
+++ config/regioes-config.js
@@ -15,7 +15,7 @@ const configuracoesRegioes = {
     id: 'rs_norte',
     nome: 'RS Norte',
     planilhaId: '1zxsWdSrPsPV6zqvNk0cv8zUosvntcZPGBMT9oLiB21s',
-    geojsonPath: 'municipios-RS_Norte.geojson', // Sintaxe corrigida
+    geojsonPath: 'data/geojson/municipios-RS_Norte.geojson', // Sintaxe corrigida
     view: [-28.5000, -53.0000],
     zoom: 7,
     marcadorIcone: 'data/rc/marcador_Jeison.svg',
@@ -29,7 +29,7 @@ const configuracoesRegioes = {
     id: 'sc',
     nome: 'SC',
     planilhaId: '1aWYSIjBBS6q6TaLlDETakqz_QUQrg3dR8nVwKYQlyHY',
-    geojsonPath: 'municipios-SC.geojson', // Sintaxe corrigida
+    geojsonPath: 'data/geojson/municipios-SC.geojson', // Sintaxe corrigida
     view: [-27.2423, -50.2189],
     zoom: 7,
     marcadorIcone: 'data/rc/marcador_Jeison.svg',
@@ -43,7 +43,7 @@ const configuracoesRegioes = {
     id: 'pr',
     nome: 'PR',
     planilhaId: '1_DlY-t96oZ5HMctUv40MjZSFPQNBkr0XmGglUQ5kBn0',
-    geojsonPath: 'municipios-PR.geojson', // Sintaxe corrigida
+    geojsonPath: 'data/geojson/municipios-PR.geojson', // Sintaxe corrigida
     view: [-24.7935, -50.0000],
     zoom: 7,
     marcadorIcone: 'data/rc/marcador_Jeison.svg',
@@ -57,7 +57,7 @@ const configuracoesRegioes = {
     id: 'sp',
     nome: 'SP',
     planilhaId: '1iIyNSJSvZO53txewSIdgulgOQr9LkKVlKC4jYdleH1U',
-    geojsonPath: 'municipios-SP.geojson', // Sintaxe corrigida
+    geojsonPath: 'data/geojson/municipios-SP.geojson', // Sintaxe corrigida
     view: [-22.1500, -48.0000],
     zoom: 7,
     marcadorIcone: 'data/rc/marcador_Andre.svg',
@@ -71,7 +71,7 @@ const configuracoesRegioes = {
     id: 'ms',
     nome: 'MS',
     planilhaId: '1UCqKvj-R5QRhRaHo6bsLGhWRiLvlw2txPQsPeHG1-rE',
-    geojsonPath: 'municipios-MS.geojson', // Sintaxe corrigida
+    geojsonPath: 'data/geojson/municipios-MS.geojson', // Sintaxe corrigida
     view: [-20.4697, -54.6201],
     zoom: 7,
     marcadorIcone: 'data/rc/marcador_Jeison.svg',
