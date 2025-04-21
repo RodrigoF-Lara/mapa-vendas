5c0a2e55f8693a9bd36cfdea3c4baaf594b7b21f
RodrigoF-Lara <rodrigo.lara@rede.ulbra.br>
2025-04-21 13:56:36 -0300

    Update regioes-config.js

--- config/regioes-config.js
+++ config/regioes-config.js
@@ -1,85 +1,23 @@
+// Configurações das regiões
 const configuracoesRegioes = {
-  'rs_sul': {
-    id: 'rs_sul',
-    nome: 'RS Sul',
-    planilhaId: '1R7cj2ajVFQTRSWLNKdY1d1JNVhAjfFfsMvIWKeIhwiA',
-    geojsonPath: 'data/geojson/municipios-RS SUL.geojson', // Nome corrigido
+  rs_sul: {
     view: [-30.0346, -51.2177],
-    zoom: 7,
+    zoom: 6,
+    planilhaId: '1BXbXYZxyz123456789', // Substitua pelo ID real da planilha
+    geojsonPath: 'data/rs_sul.geojson',
+    marcadorIcone: 'data/rc/marcador_Jeison.svg',
+    imagem: 'data/imagens/rs_sul.jpg',
     cidadesRC: {
-      '4301602': 'GUSTAVO'      
-    },
-    imagem: 'data/rc/Gustavo.PNG',
-    marcadorIcone: 'data/rc/marcador_Gustavo.svg'
+      '4300001': 'Jeison', // Substitua pelos códigos IBGE reais
+      '4300002': 'André'
+    }
   },
-  
-  'rs_norte': {
-    id: 'rs_norte',
-    nome: 'RS Norte',
-    planilhaId: '1zxsWdSrPsPV6zqvNk0cv8zUosvntcZPGBMT9oLiB21s',
-    geojsonPath: 'data/geojson/municipios-RS Norte.geojson', // Sintaxe corrigida
-    view: [-28.5000, -53.0000],
-    zoom: 7,
-    cidadesRC: {
-        '4304705': 'LEANDRO'
-     },
-    imagem: 'data/rc/Jeison.PNG',
-    marcadorIcone: 'data/rc/marcador_Jeison.svg'
+  // Mantenha as outras regiões que você já tem configuradas
+  rs_norte: {
+    // suas configurações atuais
   },
-  
-  'sc': {
-    id: 'sc',
-    nome: 'SC',
-    planilhaId: '1aWYSIjBBS6q6TaLlDETakqz_QUQrg3dR8nVwKYQlyHY',
-    geojsonPath: 'data/geojson/municipios-SC.geojson', // Sintaxe corrigida
-    view: [-27.2423, -50.2189],
-    zoom: 7,
-    cidadesRC: {
-      '4209003': 'LEONARDO'
-    },
-    imagem: 'data/rc/Jeison.PNG',
-    marcadorIcone: 'data/rc/marcador_Jeison.svg'
+  sc: {
+    // suas configurações atuais
   },
-  
-  'pr': {
-    id: 'pr',
-    nome: 'PR',
-    planilhaId: '1_DlY-t96oZ5HMctUv40MjZSFPQNBkr0XmGglUQ5kBn0',
-    geojsonPath: 'data/geojson/municipios-PR.geojson', // Sintaxe corrigida
-    view: [-24.7935, -50.0000],
-    zoom: 7,
-    cidadesRC: {
-        '4104808': 'ISRAEL'
-     },
-    imagem: 'data/rc/Jeison.PNG',
-    marcadorIcone: 'data/rc/marcador_Jeison.svg'
-  },
-  
-  'sp': {
-    id: 'sp',
-    nome: 'SP',
-    planilhaId: '1iIyNSJSvZO53txewSIdgulgOQr9LkKVlKC4jYdleH1U',
-    geojsonPath: 'data/geojson/municipios-SP.geojson', // Sintaxe corrigida
-    view: [-22.1500, -48.0000],
-    zoom: 7,
-    cidadesRC: {
-        '3529005': 'ANDRE'
-    },
-    imagem: 'data/rc/Andre.PNG',
-    marcadorIcone: 'data/rc/marcador_Andre.svg'
-  },
-  
-  'ms': {
-    id: 'ms',
-    nome: 'MS',
-    planilhaId: '1UCqKvj-R5QRhRaHo6bsLGhWRiLvlw2txPQsPeHG1-rE',
-    geojsonPath: 'data/geojson/municipios-MS.geojson', // Sintaxe corrigida
-    view: [-20.4697, -54.6201],
-    zoom: 7,
-    cidadesRC: {
-        '5003702': 'GRAZIAN'
-    },
-    imagem: 'data/rc/Jeison.PNG',
-    marcadorIcone: 'data/rc/marcador_Jeison.svg'
-  }
+  // etc.
 };
