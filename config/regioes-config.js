45a59e9ee2ff888de7855ace99ba1bb89e7e3cf3
RodrigoF-Lara <rodrigo.lara@rede.ulbra.br>
2025-04-21 13:59:18 -0300

    Update regioes-config.js

--- config/regioes-config.js
+++ config/regioes-config.js
@@ -3,21 +3,13 @@ const configuracoesRegioes = {
   rs_sul: {
     view: [-30.0346, -51.2177],
     zoom: 6,
-    planilhaId: '1BXbXYZxyz123456789', // Substitua pelo ID real da planilha
+    planilhaId: '1R7cj2ajVFQTRSWLNKdY1d1JNVhAjfFfsMvIWKeIhwiA', // Substitua pelo ID real da planilha
     geojsonPath: 'data/rs_sul.geojson',
     marcadorIcone: 'data/rc/marcador_Jeison.svg',
     imagem: 'data/imagens/rs_sul.jpg',
     cidadesRC: {
-      '4300001': 'Jeison', // Substitua pelos códigos IBGE reais
-      '4300002': 'André'
-    }
-  },
-  // Mantenha as outras regiões que você já tem configuradas
-  rs_norte: {
-    // suas configurações atuais
-  },
-  sc: {
-    // suas configurações atuais
-  },
-  // etc.
-};
+      '4301602': 'GUSTAVO' 
+    },
+    imagem: 'data/rc/Gustavo.PNG',
+    marcadorIcone: 'data/rc/marcador_Gustavo.svg'
+  }
