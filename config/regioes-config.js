4ead241293e376c1395611cf49f68bb56253460e
RodrigoF-Lara <rodrigo.lara@rede.ulbra.br>
2025-04-21 14:10:40 -0300

    Update regioes-config.js

--- config/regioes-config.js
+++ config/regioes-config.js
@@ -3,13 +3,72 @@ const configuracoesRegioes = {
   rs_sul: {
     view: [-30.0346, -51.2177],
     zoom: 6,
-    planilhaId: '1R7cj2ajVFQTRSWLNKdY1d1JNVhAjfFfsMvIWKeIhwiA', // Substitua pelo ID real da planilha
+    planilhaId: '1R7cj2ajVFQTRSWLNKdY1d1JNVhAjfFfsMvIWKeIhwiA',
     geojsonPath: 'data/rs_sul.geojson',
-    marcadorIcone: 'data/rc/marcador_Jeison.svg',
-    imagem: 'data/imagens/rs_sul.jpg',
+    marcadorIcone: 'data/rc/marcador_Gustavo.svg',
+    imagem: 'data/rc/Gustavo.PNG',
     cidadesRC: {
       '4301602': 'GUSTAVO' 
-    },
-    imagem: 'data/rc/Gustavo.PNG',
-    marcadorIcone: 'data/rc/marcador_Gustavo.svg'
+    }
+  },
+  rs_norte: {
+    view: [-28.2612, -52.4083],
+    zoom: 6,
+    planilhaId: '1ABCDEFxyz123456789',
+    geojsonPath: 'data/rs_norte.geojson',
+    marcadorIcone: 'data/rc/marcador_Andre.svg',
+    imagem: 'data/imagens/rs_norte.jpg',
+    cidadesRC: {
+      '4300003': 'André',
+      '4300004': 'Jeison'
+    }
+  },
+  sc: {
+    view: [-27.5954, -48.5480],
+    zoom: 6,
+    planilhaId: '1GHIJKLxyz123456789',
+    geojsonPath: 'data/sc.geojson',
+    marcadorIcone: 'data/rc/marcador_Jeison.svg',
+    imagem: 'data/imagens/sc.jpg',
+    cidadesRC: {
+      '4200001': 'Jeison',
+      '4200002': 'André'
+    }
+  },
+  pr: {
+    view: [-25.4284, -49.2733],
+    zoom: 6,
+    planilhaId: '1MNOPQRxyz123456789',
+    geojsonPath: 'data/pr.geojson',
+    marcadorIcone: 'data/rc/marcador_Andre.svg',
+    imagem: 'data/imagens/pr.jpg',
+    cidadesRC: {
+      '4100001': 'André',
+      '4100002': 'Jeison'
+    }
+  },
+  sp: {
+    view: [-23.5505, -46.6333],
+    zoom: 6,
+    planilhaId: '1STUVWXxyz123456789',
+    geojsonPath: 'data/sp.geojson',
+    marcadorIcone: 'data/rc/marcador_Jeison.svg',
+    imagem: 'data/imagens/sp.jpg',
+    cidadesRC: {
+      '3500001': 'Jeison',
+      '3500002': 'André'
+    }
+  },
+  ms: {
+    view: [-20.4428, -54.6464],
+    zoom: 6,
+    planilhaId: '1YZABCDxyz123456789',
+    geojsonPath: 'data/ms.geojson',
+    marcadorIcone: 'data/rc/marcador_Andre.svg',
+    imagem: 'data/imagens/ms.jpg',
+    cidadesRC: {
+      '5000001': 'André',
+      '5000002': 'Jeison'
+    }
   }
+};
